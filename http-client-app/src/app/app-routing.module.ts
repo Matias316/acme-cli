a5e4299588ed6ea05ce7d4f4112418c8e5cef8b4
Matias <mjaindu@hotmail.com>
2020-04-06 15:00:26 -0300

    Add first components

--- http-client-app/src/app/app-routing.module.ts
+++ http-client-app/src/app/app-routing.module.ts
@@ -1,8 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { SongCreateComponent } from './song-create/song-create.component'
+import { SongEditComponent } from './song-edit/song-edit.component'
+import { SongListComponent } from './song-list/song-list.component'
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: 'create-employee' },
+  { path: 'create-song', component: SongCreateComponent },
+  { path: 'song-list', component: SongListComponent },
+  { path: 'song-edit/:id', component: SongEditComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- http-client-app/src/app/app.module.ts
+++ http-client-app/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SongCreateComponent } from './song-create/song-create.component';
+import { SongEditComponent } from './song-edit/song-edit.component';
+import { SongListComponent } from './song-list/song-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SongCreateComponent,
+    SongEditComponent,
+    SongListComponent
   ],
   imports: [
     BrowserModule,
